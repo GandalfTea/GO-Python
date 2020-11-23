52ad760d0ac873a9ebd13cc6c605e4b228259e41
Rusu Octavian <octavian.concept@gmail.com>
2020-11-23 22:57:02 +0000

    stone class

---
+++ stone_class.py
@@ -0,0 +1,35 @@
+
+
+all[]
+
+class stone:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        lnb[]
+
+        all.append(self)
+
+    
+    def nb:
+        for st in all:
+            if x == st.x or y == st.y:
+                lnb.append(st)
+
+    def nb_all:
+        for st in lnb:
+            print(st)
+
+    # check neighbours
+    # store neighbours if stones
+    # if two similar coordinates, store in container.
+    # if container reaches the initial value, it is circle.
+
+
+
+
+
+
+
+
+
