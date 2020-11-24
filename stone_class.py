32a1754e3ec74cf720ace1dc92eb6d5cf03ad25f
Octavian Rusu <octavian.concept@gmail.com>
2020-11-24 10:27:04 +0000

    nb working

--- stone_class.py
+++ stone_class.py
@@ -1,35 +1,36 @@
 
 
 allst = []
-lnb = []
 
 class st:
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.nb()
+        self.lnb = self.nb()
         allst.append(self)
         print("string x :", x, "string y :", y)
 
     def nb(self):
-       for st in allst:
+        lnb = []
+        for st in allst:
            #print("I will tel you a secreat.")
            if self.x == st.x or self.y == st.y or self.x == st.y or self.y == st.x:
                 #print("I was the wierd duck")
                 lnb.append(st)
+        return lnb
 
-    def nb_all():
-        for st in lnb:
-            print(st.x, " ", st.y)
+    def nb_all(self):
+        if self.lnb is not None:
+            for st in self.lnb:
+                print(st.x, " ", st.y)
 
     # if container reaches the initial value, it is circle.
 
 a = st(3,4)
 b = st(4,5)
-st.nb_all()
-
-
+c = st(4,6)
+c.nb_all()
 
 
 
