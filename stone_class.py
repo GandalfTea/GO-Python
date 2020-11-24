fbe1bced6edbc74d9a020c6c526e22a3c11232be
Rusu Octavian <octavian.concept@gmail.com>
2020-11-24 20:22:16 +0000

    start player class

--- stone_class.py
+++ stone_class.py
@@ -1,14 +1,22 @@
 
 
 allst = []
+plst = []
 
 class st:
-    def __init__(self, x, y):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        allst.append(self)
+        
+
+    def pl(self, x, y):
         self.x = x
         self.y = y
         self.lnb = self.nb()
+        plst.append(self)
         allst.append(self)
-        print("string x :", x, "string y :", y)
+        #print("string x :", x, "string y :", y)
 
     def nb(self):
         lnb = []
@@ -17,6 +25,14 @@ class st:
            if self.x == st.x or self.y == st.y or self.x == st.y or self.y == st.x:
                 #print("I was the wierd duck")
                 lnb.append(st)
+        
+        def iter:
+            # iterating the nb:
+            for nb in lnb:
+                if nb.x == self.x and nb.y == self.y:
+                    return 1
+                nb.nb()
+
         return lnb
 
     # Debug, print all neighbours
@@ -25,12 +41,32 @@ class st:
             for st in self.lnb:
                 print(st.x, " ", st.y)
 
+
+class stplayer:
+
+    def __init__(self, color):
+        
+        if color == "b":
+            self.STNUM = 181
+        elif color == "w":
+            self.STNUM = 180
+        
+        self.container = []
+        for i in range(STNUM):
+            self.container[i] = st()
+    
+    for st in self.container:
+        if self.nb() is not None:
+            for nb in self.nb():
+                
+
+    # iterate all objects in container
+    # start a recursive string through all the objects to their neighbours
+    # if string reaches entry point, it is complete.
+    # complete string analyze the interior of the circle.
     # if container reaches the initial value, it is circle.
 
-a = st(3,4)
-b = st(4,5)
-c = st(4,6)
-c.nb_all()
+
 
 
 
