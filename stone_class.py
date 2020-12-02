debcb26461594bbe2e2c65c3e940c9c6dc2ce9fa
Rusu Octavian <octavian.concept@gmail.com>
2020-12-02 21:59:14 +0000

    feedforward

--- stone_class.py
+++ stone_class.py
@@ -1,7 +1,7 @@
 
 
-allst = []
-plst = []
+_all_st = []    #all stones
+_pl_st = []     #played stones
 
 class st:
     def __init__(self):
@@ -9,43 +9,54 @@ class st:
         self.y = 0
         self._all_nb = []
 
-        allst.append(self)
+        _all_st.append(self)
 
     def pl(self, x, y):
         self.x = x
         self.y = y
         self.lnb = self.nb()
-        plst.append(self)
-        allst.append(self)
+        _pl_st.append(self)
         #print("string x :", x, "string y :", y)
 
     def nb(self):
         lnb = []
-        for st in allst:
-           #print("I will tel you a secreat.")
-           # TODO : This does not capture neighbours 
-           if self.x == st.x or self.y == st.y or self.x == st.y or self.y == st.x:
+        for st in _all_st:
+            #print("I will tel you a secreat.")
+            if st.x == self.x and st.y == (self.y - 1):
                 #print("I was the wierd duck")
                 lnb.append(st)
-        return lnb
+            elif st.x == self.x and st.y == (self.y + 1):
+                #print("I was the wierd duck")
+                lnb.append(st)
+            elif st.x == (self.x - 1) and st.y == self.y:
+                #print("I was the wierd duck")
+                lnb.append(st)
+            elif st.x == (self.x + 1) and st.y == self.y:
+                #print("I was the wierd duck")
+                lnb.append(st)
+            else:
+                continue
+       return lnb
 
 
     def _iter(self):
         temp_nb = []    # hold stone to not iretate same again.
         hold_nb = []    # temp hold all_nb for stone
         temp_nb.append(self)
-            
-        # iter ending function     
-        for temp in temp_np:
-            if self.x == temp.x and self.y == temp.y:
-                # return
+           
+            # Propagate the function to all connected neighbours
+            def _feedforward(self, o_x, o_y, hold_nb):
+                feed_hold = []
+                if self.x == o_x and self.y == o_y:
+                    return 
 
-        # add all connecting stones to _nb_all
-        for nb in lnb:
-            if nb not in temp_nb:
-                hold_nb.append(nb.iter())
-            else:
-                return nb
+                # add all connecting stones to _nb_all
+                for nb in lnb:
+                    if nb not in temp_nb:
+                        feed_hold.append(nb)
+                    else:
+                        continue
+                return feed_hold
 
     # Debug, print all neighbours
     def nb_all(self):
