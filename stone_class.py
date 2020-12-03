2df4a2a671a60528a5fb3836a3e9fc3365a22062
Octavian Rusu <octavian.concept@gmail.com>
2020-12-03 00:07:19 +0000

    more fucking around

--- stone_class.py
+++ stone_class.py
@@ -59,27 +59,51 @@ class st:
 
         # Propagate the function to all connected neighbours
         def _feed_forward(self, o_x, o_y, temp_nb):
-            feed_hold = []
-            feed_hold.append(self)
+            for i in temp_nb:
+                if i.x != self.x and i.y != self.y:
+                    temp_nb.append(self)
+
             # End of recursion
             if self.x == o_x and self.y == o_y:
+                # Make immidiate nb not recognize you
                 print("This does not work")
-                return feed_hold
+                return []
+
+            # recalibrate the nb 
+            self.nb()
+            print("\n",self)
+            self.nb_all()
+
             # add all connecting stones to _nb_all
             for nb in self.lnb:
+                print("\nBeginnning")
+                print("temp : ", temp_nb)
+                print("lnb :", self.lnb)
                 if nb not in temp_nb:
-                    feed_hold.append(_feedforward(nb, oX, oY, hold_nb))
+                    print("ffff")
+                    for i in _feed_forward(nb, oX, oY, holf_nb):
+                        hold_nb.append(i)
+                        holf_nb.append("dicks")
                     print("currently at : ", nb)
                 else:
                     continue
-            return feed_hold
+
+                print("DICKS")
+
+            return
 
         for nb in self.lnb:
-            hold_nb.append(_feed_forward(nb, oX, oY, hold_nb))
-        
-        print("\n_iter:")
-        for i in temp_nb:
-            print(i)
+            _feed_forward(nb, oX, oY, temp_nb)
+
+       # DEBUG
+      #  print("\n_iter:")
+       # if temp_nb is None:
+        #    print("Dicks")
+        #else:
+         #   for i in temp_nb:
+          #      print(i)
+
+
 
     # Debug, print all neighbours
     def nb_all(self):
@@ -95,7 +119,16 @@ a.pl(2,5)
 b.pl(2,4)
 c.pl(1,4)
 d.pl(3,4)
+b._iter()
+print("\n A:")
+a.nb_all()
+print("\n B:")
 b.nb_all()
+print("\n C:")
+c.nb_all()
+print("\n D:")
+d.nb_all()
+
 class st_player:
     
     def __init__(self, color):
