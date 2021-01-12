accc20661188b27b191c4b7aa117a5c1d66a8f3f
Rusu Octavian <octavian.concept@gmail.com>
2021-01-12 13:03:08 -0700

    add how_to_play.py structure

---
+++ src/how_to_play.py
@@ -0,0 +1,28 @@
+
+
+# How to play window
+
+
+#   Window :
+# Create a trasparent borderless window.
+# Skip button closes the window.
+# Continue button furthers the presentation.
+# Back button
+
+#   Slide :
+
+# Video showing move
+# Text explaining rules.
+
+
+#   Order of slideshow :
+
+# 1. How to place a stone.
+# 2. Game mechanics (order of play).
+# 3. Single capture.
+# 4. Group Capture.
+# 5. Concept of eyes.
+# 6. Alive vs dead string.
+# 7. Ko rule.
+# 8. Resign.
+# 9. Count teritory.
